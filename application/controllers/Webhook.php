66bc73e6e502d99688015ffb806846209dfb033a
naufalols <muh.naufal@hotmail.com>
2021-10-08 21:26:01 +0700

    test running class

---
+++ application/controllers/Webhook.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Webhook extends CI_Controller
+{
+    public function index()
+    {
+        echo "mantap";
+    }
+}
