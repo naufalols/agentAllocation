d36d9bbd301a66c009559f2b5a9eebe4d0ddb0a2
naufalols <muh.naufal@hotmail.com>
2021-10-11 00:10:46 +0700

    add ngrok and test connection with auth

---
+++ application/helpers/webhook_helper.php
@@ -0,0 +1,27 @@
+<?php
+
+if (! defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+function curl_qiscus_auth()
+{
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://multichannel.qiscus.com/api/v1/auth',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array('email' => 'muh.naufal@hotmail.com','password' => 'Sampoerna9'),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    echo $response;
+}
